a2f739502fba8eef91ec011295fd7d4643b3daef
Gaurav Kumar <rkroy39@gmail.com>
2023-01-09 23:11:38 +0530

    home

--- src/basicprograms/BasicPrograms.java
+++ src/basicprograms/BasicPrograms.java
@@ -1,9 +1,16 @@
 package basicprograms;
 
 public class BasicPrograms {
+	
+	void addNumbers(int a,int b) {
+		System.out.println("Addition is "+(a+b));
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		
+		BasicPrograms bp=new BasicPrograms();
+		bp.addNumbers(20,30);
 	}
 
 }
